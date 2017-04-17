084527ec41c3f82d6b4ad782b8ac6240ab298851
Deborah Agumeneitwe <dagumeneitwe@gmail.com>
2017-04-17 15:39:52 +0300

    Initialises the project

---
+++ breakfast.py
@@ -0,0 +1,47 @@
+""""print ("Get the ingredients")
+print ("mix")
+print ("pour into heated pan")
+print ("flip the egg to the other side")
+breakfast = "-------yummy egg------"
+print (breakfast)
+
+
+print ("Get the ingredients")
+print ("mix")
+print ("pour into heated pan")
+print ("flip the egg to the other side")
+breakfast = "-------yummy pancake------"
+print (breakfast)"""
+
+"""def breakfast(food,ingredients):
+    #ingredients = []
+    #food = input("what type of breakfast will you have? ",food)
+    print ("Get the ingredients")
+    #ingredients = input("ingredients include ")
+    print ("mix")
+    print ("pour into heated pan")
+    print ("flip the egg to the other side")
+    breakfast = "-------yummy"+ food + "------"  
+    return breakfast"""
+
+#print (breakfast("egg"))
+#print (breakfast("pancake"))
+
+ingredients = ["bacon","tomatoes"]
+def mix_and_cook(food):
+    print ("Get the ingredients")
+    print ("mix")
+    print ("pour into heated pan")
+    print ("flip the egg to the other side")
+    #breakfast = "-------yummy pancake served with" + ""------"
+    
+def breakfast(food,ingredients):
+    if (len(ingredients) == 0):
+        print ("yummy {}".food.format)
+        
+    else:
+        print ("yummy" + food+  "served with"+ ingredients )
+        
+         
+    
+    
