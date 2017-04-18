6c7af1ad7b3d660d1b4f00c96eb7f9720dd5079e
Deborah Agumeneitwe <dagumeneitwe@gmail.com>
2017-04-18 12:50:47 +0300

    edited test to prime numbers

--- prime_number/prime_numbers_test.py
+++ prime_number/prime_numbers_test.py
@@ -7,7 +7,7 @@ class TestPrimeGenerator(unittest.TestCase):
         self.test1 = is_prime_number(-1)
         self.test2 = is_prime_number(1)
         self.test3 = is_prime_number(1066340417491710595814572169)
-        #self.test4 = is_prime_number(1066340417491710595814572168)
+        self.test4 = is_prime_number(10668)
         self.test5 = is_prime_number(5)
         self.test6 = is_prime_number("boy")
     
@@ -23,15 +23,16 @@ class TestPrimeGenerator(unittest.TestCase):
     def test_5_is_prime(self):
 		self.assertTrue(self.test5)
         
+     def test_massive_even_number_is_not_prime(self):
+		self.assertTrue(self.test4)
         
     def test_invalid_type_string(self):
         self.assertTrue(self.test6)
         
         
-    """
     
-    def test_massive_even_number_is_not_prime(self):
-		self.assertFalse(self.test4)"""
+    
+   
     
 
 
