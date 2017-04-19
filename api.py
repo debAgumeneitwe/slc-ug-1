b1f141a05faf9f198e4134ddb294da38ba1a7092
Deborah Agumeneitwe <dagumeneitwe@gmail.com>
2017-04-19 17:04:39 +0300

    API

--- api.py
+++ api.py
@@ -1,19 +1,21 @@
 import requests
 import json
 
-openweathermap_api = '030b9098987b9ee49faeaef6fe11f4d8'
+OpenWeatherMap_api = '030b9098987b9ee49faeaef6fe11f4d8'
 
 url = 'http://api.openweathermap.org/data/2.5/weather?id=184742&appid=030b9098987b9ee49faeaef6fe11f4d8'
 json_obj = requests.get(url)
  
-data = json.load(json_obj.text)
+data = json.loads(json_obj.text)
+
+print(data['main'])
+print(data['coord']) 
+print(data['name'])
+print (data['weather'])
 
 
-for item in data:
-    print (item['coord'])
-    print (item['country'])
-    print (item('id'))
-    print (item['name'])
+
+    
     
 
        
