8052b6f571a092f5e8e70d73bb8b4af03a96b593
Deborah Agumeneitwe <dagumeneitwe@gmail.com>
2017-04-22 16:48:18 +0300

    debugged car.py

--- car.py
+++ car.py
@@ -1,34 +1,30 @@
 class Car(object):
-    def _init_(self, name='General', model='GM', type='Car'):
+    def __init__(self, name='General', model='GM', car_type='Saloon'):
         self.name = name
         self.model = model
-        self.type = type
+        self.car_type = car_type
         self.speed = 0
-
         if self.name == 'Porshe' or self.name == 'Koenigsegg':
             self.num_of_doors = 2
         else:
             self.num_of_doors = 4
-
-        if self.type != 'trailer':
+        if self.car_type != 'trailer':
             self.num_of_wheels = 4
         else:
             self.num_of_wheels = 8
-
     def is_saloon(self):
-        if self.type != 'trailer':
+        if self.car_type != 'trailer':
             return True
         else:
             return False
-
     def drive(self, num):
         """drive car by altering speed
-
         this method as returns the updated car object.
         """
-        if self.type == 'Car':
+        if self.car_type == 'Saloon':
             self.speed = 10 ** num
         else:
             self.speed = 77
-
         return self
+    
+    
